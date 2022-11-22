2a1695072f568c2c6fb68020c53a97045ff35469
BilgeGngr <79796386+Bilgegngr@users.noreply.github.com>
2022-11-22 21:21:54 +0300

    odev5

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,77 +2,73 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa = function (req, res, next) {
-  res.render('anasayfa',
-   { "baslik": "Ana Sayfa",
-     "sayfaBaslik":{
+  res.render('anasayfa', 
+  { "baslik": "Ana sayfa",
+    "sayfaBaslik":{
       "siteAd":"Mekanbul",
-      "slogan":"Civardaki Mekanları Keşfet!"
-     },
-     "mekanlar":[
-
+      "slogan":"Civardaki Mekanları Keşfet !"
+    },
+    "mekanlar":[
       {
         "ad":"Barida Kafe",
-        "adres":"Sdü Batı Kampüs",
+        "adres":"Sdü Batı kampüsü",
         "puan":"4",
         "mesafe":"2km",
-        "imkanlar":["kahve","Çay","Pasta"]
+        "imkanlar":["Kahve","Çay","Pasta"]
       },
       {
-        "ad":"Gloria Kafe",
-        "adres":"Sdü Doğu Kampüs",
-        "puan":"4",
+        "ad":"Gloria",
+        "adres":"Sdü Doğu kampüsü",
+        "puan":"2",
         "mesafe":"5km",
-        "imkanlar":["kahve","Çay","Pasta"]
-      },
-     ]
-     
-  
-  
+        "imkanlar":["Kahve","Çay","Pasta"]
+      }
+
+    ]
+
 });
 }
 
 const mekanBilgisi = function (req, res) {
-  res.render('mekanbilgisi',
-  { "baslik": "Mekan bilgisi", 
+  res.render('mekanbilgisi', 
+  { "baslik": "Mekan bilgisi" ,
     "mekanBaslik":"Starbucks",
     "mekanDetay":{
       "ad":"Starbucks",
       "adres":"Centrum Garden",
       "puan":"4",
-      "saatler":
-      [
+      "saatler":[
         {
-        "gunler":"pazartesi - cuma",
-        "acilis":"9.00",
-        "kapanis":"23.00",
-        "kapali":false
-      },
-      {
-        "gunler":"cumartesi - pazar",
-        "acilis":"10.00",
-        "kapanis":"23.00",
-        "kapali":false
-      }
+          "gunler":"Pazartesi-Cuma",
+          "acilis":"9:00",
+          "kapanis":"23:00",
+          "kapali":false
+        },
+        {
+          "gunler":"Cumartesi-Pazar",
+          "acilis":"10:00",
+          "kapanis":"22:00",
+          "kapali":false
+        }
       ],
-      "imkanlar":["kahve","çay","kek"],
+      "imkanlar":["Kahve","Çay","Kek"],
       "koordinatlar":{
         "enlem":"37.7",
         "boylam":"30.5"
       },
       "yorumlar":[
         {
-          "yorumYapan":"Bilge Güngör",
+          "yorumYapan":"Sinan",
           "puan":"3",
-          "tarih":"22 Mayıs",
-          "yorumMetni":" Çok beğendim"
+          "tarih":"20 Ekim 2022",
+          "yorumMetni":"Çok berbat."
         },
         {
-          "yorumYapan":"Osman",
-          "puan":"3",
-          "tarih":"19 Ocak",
-          "yorumMetni":" Çok beğendim"
+          "yorumYapan":"Bilge",
+          "puan":"5",
+          "tarih":"20 Ekim 2022",
+          "yorumMetni":"Güzel."
         }
-        
       ]
     }
 
@@ -83,10 +79,9 @@ const yorumEkle = function (req, res) {
   res.render('yorumekle', { "title": "Yorum ekle" });
 }
 
-module.exports = {
 
+module.exports = {
   anaSayfa,
   mekanBilgisi,
   yorumEkle
-
 }
